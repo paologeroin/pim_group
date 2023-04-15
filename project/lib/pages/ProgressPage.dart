a418ffe42209d0f37b8942ecc5cc7d77c4513e6d
MariaDalleVacche <maria.dallevacche@studenti.unipd.it>
2023-04-16 00:07:42 +0200

    Added ProgressPage class

---
+++ project/lib/pages/ProgressPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProgressPage extends StatefulWidget {
+  const ProgressPage({super.key});
+
+  @override
+  State<ProgressPage> createState() => _ProgressState();
+}
+
+class _ProgressState extends State<ProgressPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
