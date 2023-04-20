9e4cdef86f5a43dc762f8bacf0402cf0e9a4e94d
“Irene <irene.tassinato@studenti.unipd.it>
2023-04-20 18:25:18 +0200

    small modification of ProfilePage.dart

--- project/lib/pages/ProfilePage.dart
+++ project/lib/pages/ProfilePage.dart
@@ -6,11 +6,27 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(title: const Text('Profile')),
-      body: Center(
-        child: Text('Profile page')
-        
-      )
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        iconTheme: const IconThemeData(color: Color.fromARGB(145, 0, 255, 94)),
+        title: const Text('Profilo', style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
+      ),
+      body: SingleChildScrollView(
+        child: const Text('Attendo spiegazione tutor'),
+      ),
     );
   }
+  // vacchio codice
+  // @override
+  // Widget build(BuildContext context) {
+  //   return  Scaffold(
+  //     appBar: AppBar(title: const Text('Profile')),
+  //     body: Center(
+  //       child: Text('Profile page')
+        
+  //     )
+  //   );
+  // }
 }
