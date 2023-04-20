cc04e13c0a81b46d2ec9a17f08982e1e3b91af70
MariaDalleVacche <maria.dallevacche@studenti.unipd.it>
2023-04-20 17:21:22 +0200

    changed class name and list name

--- project/lib/models/drinks.dart
+++ project/lib/models/drinks.dart
@@ -1,7 +1,7 @@
 //This class acts as data repository for the drinks catalog of what the user can drink.
-class Catalog {
+class DrinksCatalog {
   //A drink is represented as a String (this is the method that the professor used in the lesson, we can change it I think (Paolo))
-  List<String> products = [
+  List<String> drinks = [
     'Beer',
     'Spritz',
     'Glass of Wine',
