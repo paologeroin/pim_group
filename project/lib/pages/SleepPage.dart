cfe4cda5276aef962b8ff25a145e6566bf7ec696
“Irene <irene.tassinato@studenti.unipd.it>
2023-05-01 17:07:35 +0200

    some changes to SleepPage

--- project/lib/pages/SleepPage.dart
+++ project/lib/pages/SleepPage.dart
@@ -12,7 +12,7 @@ class _SleepPageState extends State<SleepPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text ('Esempio per costruzione Grafico', 
+      appBar: AppBar(title: const Text ('Pagina relativa al sonno', 
       textAlign: TextAlign.center), 
       centerTitle: true,
       leading: IconButton(
@@ -25,41 +25,65 @@ class _SleepPageState extends State<SleepPage> {
         ),
       ),
       ),
-      body: Center(
-        child: Container(
-            //Initialize chart
-            child: SfCartesianChart(
-              // Chart title text
-              title: ChartTitle(text: 'Half yearly sales analysis'),
-              // Enables the legend
-              legend: Legend(isVisible: true),
-              // Initialize category axis
-              primaryXAxis: CategoryAxis(),
-              series: <ChartSeries>[
-                // Initialize line series
-                StepLineSeries<ChartData, int>(
-                  // Enables the tooltip for individual series
-                  enableTooltip: true, 
-                  dataSource: [
-                      // Bind data source
-                      ChartData(2010, 32),
-                      ChartData(2011, 40),
-                      ChartData(2012, 34),
-                      ChartData(2013, 52),
-                      ChartData(2014, 42),
-                      ChartData(2015, 38),
-                      ChartData(2016, 41),
-                  ],
-                  xValueMapper: (ChartData data, _) => data.x,
-                  yValueMapper: (ChartData data, _) => data.y,
-                  // Render the data label
-                  dataLabelSettings: DataLabelSettings(isVisible : true),
-                  )
-            ])
+      body: Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+          child: ListView(
+            children: [
+              Text(
+                "Your Sleep",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [ 
+                  IconButton( //freccia di sinistra
+                      icon: const Icon(Icons.navigate_before),
+                      onPressed: () {}),
+                  Text('Data di oggi'),
+                  IconButton(
+                      icon: const Icon(Icons.navigate_next),
+                      onPressed: () {})
+                ],
+              ),
+              Center(
+                child: Container(
+                    //Initialize chart
+                    child: SfCartesianChart(
+                      // Chart title text
+                      title: ChartTitle(text: 'Last night sleep'),
+                      // Enables the legend
+                      legend: Legend(isVisible: false),
+                      // Initialize category axis
+                      primaryXAxis: CategoryAxis(),
+                      series: <ChartSeries>[
+                        // Initialize line series
+                        StepLineSeries<ChartData, int>(
+                          // Enables the tooltip for individual series
+                          enableTooltip: true, 
+                          dataSource: [
+                              // Bind data source
+                              ChartData(21, 0),
+                              ChartData(23, -10),
+                              ChartData(1, 15),
+                              ChartData(3, -5),
+                              ChartData(5, 0),
+                              ChartData(7, 10),
+                              ChartData(9, -5),
+                          ],
+                          xValueMapper: (ChartData data, _) => data.x,
+                          yValueMapper: (ChartData data, _) => data.y,
+                          // Render the data label
+                          dataLabelSettings: DataLabelSettings(isVisible : true),
+                          )
+                    ])
+                )
         )
-      )   
-    );
-  }
+      ])
+    )
+  );}
 }
 
 class ChartData {
