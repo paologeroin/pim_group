3d8009e40e42274f5dc59311e06bbc861dd4e419
MariaDalleVacche <maria.dallevacche@studenti.unipd.it>
2023-05-02 13:52:37 +0200

    add catalog page for adding drinks (to be updated)

---
+++ project/lib/models/contacts.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+// menu a tendina
+class MyWidget extends StatelessWidget {
+  String? currentSelectedValue;
+
+   final ValueNotifier<List<String>> _listNotifier =
+      ValueNotifier<List<String>>(["Beer", "Wine", "Cocktail"]);
+
+  String? getCurrentValue(){
+    return currentSelectedValue;
+  }
+  
+  Widget typeFieldWidget() {
+    return ValueListenableBuilder(
+      valueListenable: _listNotifier,
+      builder: (BuildContext context, List<String> list, Widget? child) {
+        return Container(
+             
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          
+          child: FormField<String>(
+            builder: (FormFieldState<String> state) {
+              return InputDecorator(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    hint: Text("Select Drink"),
+                    value: currentSelectedValue,
+                    isDense: true,
+                    onChanged: (newValue) {
+                        currentSelectedValue = newValue;
+                      _listNotifier.notifyListeners();
+                    },
+                    items: list.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          
+      child: typeFieldWidget(),
+    ));
+  }
+}
+
