b4ece36cedd101f43bda1eaec91fc1e7703be90e
“Irene <irene.tassinato@studenti.unipd.it>
2023-05-18 18:07:11 +0200

    some modification to the sleep_provider & sleepDB

--- project/lib/models/sleep/sleepDB.dart
+++ project/lib/models/sleep/sleepDB.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:pim_group/models/sleep/sleep_provider.dart';
+// import 'package:pim_group/models/sleep/sleep_provider.dart';
 
 // Refer to lab_04-hello_flutter for list of fitbit sleep data
 // dateOfSleep: day associated to the sleep entry (MM-DD format)
@@ -12,43 +14,49 @@ class DateOfSleep {
 // startTime: the starting timestamp of the sleep (MM-DD hh:mm:ss format)
 class StartTime {
   final DateTime startTime;
+  final DateTime timestamp;
 
-  StartTime(this.startTime);
+  StartTime(this.startTime, this.timestamp);
 } //StartTime
 
 // endTime: the ending timestamp of the sleep (MM-DD hh:mm:ss format)
 class EndTime {
   final DateTime endTime;
+  final DateTime timestamp;
 
-  EndTime(this.endTime);
+  EndTime(this.endTime, this.timestamp);
 }
 
 // duration: the duration of the sleep session (in ms)
-class Duration {
-  final int duration;
+class sleepDuration {
+  final int durationValue;
+  final DateTime timestamp;
 
-  Duration(this.duration);
+  sleepDuration(this.durationValue, this.timestamp);
 } //Duration
 
 // minutesAsleep: the number of minutes asleep during the sleep entry
 class MinutesAsleep {
-  final int minutesAsleep; //penso sia DateTime
+  final int minutesAsleep;
+  final DateTime timestamp;
 
-  MinutesAsleep(this.minutesAsleep); //dà errore se metto required
+  MinutesAsleep(this.minutesAsleep, this.timestamp); //dà errore se metto required
 } //MinutesAsleep
 
 // minutesAwake: the number of minutes awake during the sleep entry
 class MinutesAwake {
-  final int minutesAwake; //penso sia DateTime
+  final int minutesAwake;
+  final DateTime timestamp;
 
-  MinutesAwake(this.minutesAwake); //dà errore se metto required
+  MinutesAwake(this.minutesAwake, this.timestamp); //dà errore se metto required
 } //MinutesAwake
 
 // efficiency: the estimated sleep efficiency (from 0 to 100)
 class Efficiency {
-  final int efficiency; //penso sia DateTime
+  final int efficiency; 
+  final DateTime timestamp;
 
-  Efficiency(this.efficiency); //dà errore se metto required
+  Efficiency(this.efficiency, this.timestamp); //dà errore se metto required
 }//Efficiency
 
 // levels: a summary of the sleeping stages (deep/wake/light/rem/restless)
@@ -56,3 +64,18 @@ class Efficiency {
 
 // data: the stage profile of the sleep session. Each entry contains the
 // starting timestamp, the level, and how much it lasted.
+
+
+class FitbitGen {
+  final Random _random = Random();
+
+  // List<sleepDuration> fetchSleepDuration() {
+  //   return List.generate(
+  //       100,
+  //       (index) => sleepDuration(
+  //         durationValue: _random.nextInt(180),
+  //         timestamp: DateTime.now().subtract(Duration(hours: index))
+  //       )
+  //   );
+  // }
+}
--- project/lib/models/sleep/sleep_provider.dart
+++ project/lib/models/sleep/sleep_provider.dart
@@ -1,45 +1,81 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-// import 'package:pim_group/models/sleepDB.dart';
+import 'package:pim_group/models/sleep/sleepDB.dart';
 
 // this is the change notifier. it will manage all the logic of the home page:
 // fetching the correct data from the database and on
 // startup fetching the data from the online services
 class SleepProvider extends ChangeNotifier{
-// Data to be used by the UI
-// There will be a privite part that is used only by the sleep_provider
+  // Data to be used by the UI
+  late List<StartTime> startTime;
+  late List<EndTime> endTime;
+  late List<sleepDuration> durationValue;
+  late List<MinutesAsleep> minutesAsleep;
+  late List<MinutesAwake> minutesAwake;
+  late int efficiency;
+  // There will be a privite part that is used only by the sleep_provider
+  // data fetched from external services or db
+  late List<StartTime> _startTimeDB;
+  late List<EndTime> _endTimeDB;
+  late List<sleepDuration> _durationValueDB;
+  late List<MinutesAsleep> _minutesAsleepDB;
+  late List<MinutesAwake> _minutesAwakeDB;
+  late int _efficiencyDB; //serve?
+  // selected day of data to be shown
+  DateTime dateOfSleep = DateTime.now();
+  
+  // data generators faking external services
+  final FitbitGen fitbitGen = FitbitGen(); // genera dati casuali di duration
+  final Random _random = Random();
+
+  // constructor of provider which manages the fetching of all data from the servers and then notifies the ui to build
+  // HomeProvider() {
+  //   _fetchAndCalculate();
+  //   getDataOfDay(dateOfSleep);
+  // }
+
+  // method to fetch all data and calculate the exposure
+  // void _fetchAndCalculate() {
+  //   _durationValueDB = fitbitGen.fetchSleepDuration();
+  // }
+
+  // method to trigger a new data fetching
+  // void refresh() {
+  //   _fetchAndCalculate();
+  //   getDataOfDay(dateOfSleep);
+  // }
+
+  // Constructor of provider which manages the fetching of all data from th
+  // servers and then notifies the ui to build
 
 
-// Constructor of provider which manages the fetching of all data from th
-// servers and then notifies the ui to build
+  // Methods
 
 
-// Methods
-
-
-// sistemare questa parte
-  // // method to select only the data of the chosen day
-  // // Oltre che un sorter è anche un filter
-  // void getDataOfDay(DateTime showDate) {
-  //   this.showDate = showDate;
-  //   heartRates = _heartRatesDB
-  //       .where((element) => element.timestamp.day == showDate.day)
-  //       .toList()
-  //       .reversed
-  //       .toList();
-  //   pm25 = _pm25DB
-  //       .where((element) => element.timestamp.day == showDate.day)
-  //       .toList()
-  //       .reversed
-  //       .toList();
-  //   exposure = _exposureDB
-  //       .where((element) => element.timestamp.day == showDate.day)
-  //       .toList()
-  //       .reversed
-  //       .toList();
-  //   fullexposure = exposure.map((e) => e.value).reduce(
-  //         (value, element) => value + element,
-  //       );
-  //   // after selecting all data we notify all consumers to rebuild
-  //  notifyListeners(); 
+  // sistemare questa parte
+    // method to select only the data of the chosen day
+    // Oltre che un sorter è anche un filter
+    void getDataOfDay(DateTime dateOfSleep) {
+      this.dateOfSleep = dateOfSleep;
+      durationValue = _durationValueDB
+          .where((element) => element.timestamp.day == dateOfSleep.day)
+          .toList()
+          .reversed
+          .toList();
+      // pm25 = _pm25DB
+      //     .where((element) => element.timestamp.day == showDate.day)
+      //     .toList()
+      //     .reversed
+      //     .toList();
+      // exposure = _exposureDB
+      //     .where((element) => element.timestamp.day == showDate.day)
+      //     .toList()
+      //     .reversed
+      //     .toList();
+      // fullexposure = exposure.map((e) => e.value).reduce(
+      //       (value, element) => value + element,
+      //     );
+      // after selecting all data we notify all consumers to rebuild
+     notifyListeners(); 
+    }
 } //SleepProvider
