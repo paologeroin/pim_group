fbe3b176aa124059c594f53b6fabaf8200162783
“Irene <irene.tassinato@studenti.unipd.it>
2023-05-18 18:02:35 +0200

    modification to server_strings

--- project/lib/utils/server_strings.dart
+++ project/lib/utils/server_strings.dart
@@ -10,13 +10,13 @@ class ServerStrings {
   static const authServerUrl = 'gate/v1/';
   static const apiServerUrl = 'api/v1/';
 
-/* TOLTE PERCHE CREDO NON SERVANO
+// TOLTE PERCHE CREDO NON SERVANO
 
   static String baseUrl = 'https://impact.dei.unipd.it/bwthw/';
   static String pingEndpoint = 'gate/v1/ping/';
   static String tokenEndpoint = 'gate/v1/token/';
   static String refreshEndpoint = 'gate/v1/refresh/';
 
-  // static String username = '<YOUR_USERNAME>';
-  // static String password = '<YOUR_PASSWORD>';
+  static String username = '<YOUR_USERNAME>';
+  static String password = '<YOUR_PASSWORD>';
 }//Impact
