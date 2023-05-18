90d935178b9061d6682bf93d0176f3cbc5ed5225
“Irene <irene.tassinato@studenti.unipd.it>
2023-05-18 16:07:06 +0200

    my commit

--- project/lib/utils/server_strings.dart
+++ project/lib/utils/server_strings.dart
@@ -17,6 +17,6 @@ class ServerStrings {
   static String tokenEndpoint = 'gate/v1/token/';
   static String refreshEndpoint = 'gate/v1/refresh/';
 
-  static String username = '<YOUR_USERNAME>';
-  static String password = '<YOUR_PASSWORD>'; */
+  // static String username = '<YOUR_USERNAME>';
+  // static String password = '<YOUR_PASSWORD>';
 }//Impact
